9668b7611a894de6ca69c187edd8250be3a40564
Einstein Orchestration team <mtrottiermcdonald@salesforce.com>
2024-09-12 20:57:11 -0700

    Started migrating to 2.5D

---
+++ scripts/controller3D.gd
@@ -0,0 +1,4 @@
+class_name Controller3D
+extends Node3D
+
+@export var player: Player3D
---
+++ scripts/player_3D.gd
@@ -0,0 +1,28 @@
+class_name Player3D
+extends CharacterBody3D
+
+# Nodes controlled by this node
+@export var sprite: AnimatedSprite3D
+
+# Dynamic properties
+var can_run_backward: bool = false
+
+# signals
+signal facing_left()
+signal facing_right()
+
+func _physics_process(delta: float) -> void:
+	print(self.global_position)
+
+
+#=======================================================
+# RECEIVED SIGNALS
+#=======================================================
+
+# Signal transmission from abilities
+func _on_facing_left() -> void:
+	facing_left.emit()
+
+
+func _on_facing_right() -> void:
+	facing_right.emit()
