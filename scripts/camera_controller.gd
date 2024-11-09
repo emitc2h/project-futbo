c86a65d0a57ab46dfd11bf4b055e3631b21e0c47
Michel Trottier-McDonald <mtrottiermcdonald@salesforce.com>
2024-11-08 20:56:27 -0800

    adding class building

--- scripts/camera_controller.gd
+++ scripts/camera_controller.gd
@@ -6,12 +6,10 @@ extends Node3D
 @onready var camera: Camera3D = $Camera3D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	global_position = subject.global_position
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	global_position.x = lerp(global_position.x, subject.global_position.x, 0.08)
 	global_position.y = lerp(global_position.y, subject.global_position.y, 0.1)
